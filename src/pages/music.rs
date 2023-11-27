75fa923433b5950cd69de437b481591204b613f3
macaveiAna <83480446+macaveiAna@users.noreply.github.com>
2023-11-26 20:39:56 -0700

    I will try to find another way to post the song

--- src/pages/music.rs
+++ src/pages/music.rs
@@ -8,7 +8,14 @@ impl HandleMusic {
     pub async fn music(hb: web::Data<Handlebars<'_>>) -> HttpResponse {
         //my logic for the music page
         let music_content = json!({
-            "content": "Music goes here...",
+            "content": [
+                {
+                    "name": "Yes! (feat. Jack Liebeck)",
+                    "artist": "Dario Marianelli, Jack Liebeck, Benjamin Wallfisch",
+                    "spotify_link": "https://open.spotify.com/track/2AhTppjasVXtLuMGZkoXyV?si=88cbe7efb6f54a3c"
+
+                },
+            ] 
         });
         let body =hb.render("music", &music_content).unwrap();
         HttpResponse::Ok().body(body)
