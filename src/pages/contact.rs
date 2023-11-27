147f48cd1cd169ae53a5b1b0f4af8202b730f81d
macaveiAna <83480446+macaveiAna@users.noreply.github.com>
2023-11-27 12:23:55 -0700

    form submission

--- src/pages/contact.rs
+++ src/pages/contact.rs
@@ -3,3 +3,19 @@ use handlebars::Handlebars;
 
 pub struct HandleContact;
 
+impl HandleContact {
+    pub async fn show_form(hb: web::Data<Handlebars<'_>>) -> HttpResponse {
+        let form_content = "Contact Me";
+        let body = hb.render("contact_form", &form_content).unwrap();
+        HttpResponse::Ok().body(body)
+    }
+
+    // Form submission function
+    pub async fn submit_form() -> HttpResponse {
+
+        //redirect user to a thank-you page
+        HttpResponse::SeeOther()
+            .header(actix_web::http::header::LOCATION, "/thank-you")
+            .finish()
+    }
+}
