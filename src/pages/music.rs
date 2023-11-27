c44fb4e2a2ba66640a1c16b1b55e2c60694a8f40
macaveiAna <83480446+macaveiAna@users.noreply.github.com>
2023-11-26 21:25:13 -0700

    more music

--- src/pages/music.rs
+++ src/pages/music.rs
@@ -26,7 +26,13 @@ impl HandleMusic {
                     "artist": "Switchfoot",
                     "spotify_link": "https://open.spotify.com/track/1f7qZrOScGlIcjWpfRrmSI?si=ea59bacea7ed4bbd"
 
+                },
+                {
+                    "name": "The Burning Bush",
+                    "artist":"Hans Zimmer",
+                    "spotify_link":"https://open.spotify.com/track/5tgyHc2LLF0BI2udBTMFJz?si=5574abea42d44d87"
                 }
+
             ] 
         });
         let body =hb.render("music", &music_content).unwrap();
